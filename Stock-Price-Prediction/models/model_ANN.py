d2df95d94f80309f84744859920985e0a2f3a2fe
Varshith-Yadav <varshithlavanya67@gmail.com>
2025-11-10 00:27:24 +0530

    lstm

--- Stock-Price-Prediction/models/model_ANN.py
+++ Stock-Price-Prediction/models/model_ANN.py
@@ -17,3 +17,18 @@ def build_ann(input_shape):
     return model
 
 
+
+# adding lstm version
+
+
+def build_lstm(input_shape):
+    model = Sequential([
+        LSTM(64, return_sequences=True, input_shape=input_shape),
+        Dropout(0.2),
+        LSTM(32),
+        Dropout(0.1),
+        Dense(16, activation='relu'),
+        Dense(1, activation='linear')
+    ])
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    return model
